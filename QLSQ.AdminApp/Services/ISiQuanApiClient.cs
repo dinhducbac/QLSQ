41e3c5ee9846679e5a6e7ac64dd7c1095213a321
dinhducbac <63286941+dinhducbac@users.noreply.github.com>
2021-03-29 14:55:08 +0700

    done show list

---
+++ QLSQ.AdminApp/Services/ISiQuanApiClient.cs
@@ -0,0 +1,14 @@
+﻿using QLSQ.ViewModel.Catalogs.SiQuan;
+using QLSQ.ViewModel.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QLSQ.AdminApp.Services
+{
+    public interface ISiQuanApiClient
+    {
+        public Task<APIResult<PageResult<SiQuanViewModel>>> GetAllManagePaging(GetManageSiQuanPagingRequest request);
+    }
+}
---
+++ QLSQ.AdminApp/Services/SiQuanApiClient.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using QLSQ.ViewModel.Catalogs.SiQuan;
+using QLSQ.ViewModel.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace QLSQ.AdminApp.Services
+{
+    public class SiQuanApiClient : ISiQuanApiClient
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+        private IConfiguration _configuration;
+        private IHttpContextAccessor _httpContextAccessor;
+        public SiQuanApiClient(IHttpClientFactory httpClientFactory, IConfiguration configuration,
+            IHttpContextAccessor httpContextAccessor)
+        {
+            _httpClientFactory = httpClientFactory;
+            _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
+        }
+        public async Task<APIResult<PageResult<SiQuanViewModel>>> GetAllManagePaging(GetManageSiQuanPagingRequest request)
+        {
+            var client = _httpClientFactory.CreateClient();
+            client.BaseAddress = new Uri(_configuration["BaseAddress"]);
+            var session = _httpContextAccessor.HttpContext.Session.GetString("Token");
+            client.DefaultRequestHeaders.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", session);
+            var reponse = await client.GetAsync($"/api/SiQuans/manage-paging?pageIndex={request.pageIndex}&pageSize={request.pageSize}&keyword={request.keyword}");
+            var body = await reponse.Content.ReadAsStringAsync();
+            var user = JsonConvert.DeserializeObject<APIResult<PageResult<SiQuanViewModel>>>(body);
+            return user;
+        }
+    }
+}
