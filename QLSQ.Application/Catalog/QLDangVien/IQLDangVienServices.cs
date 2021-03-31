311997ce5022aadacd5aedcf3ae8abde61386d5b
dinhducbac <63286941+dinhducbac@users.noreply.github.com>
2021-03-31 11:45:03 +0700

    create show list QLDangVien

---
+++ QLSQ.Application/Catalog/QLDangVien/IQLDangVienServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using QLSQ.ViewModel.Catalogs.QLDangVien;
+using QLSQ.ViewModel.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSQ.Application.Catalog.QLDangVien
+{
+    public interface IQLDangVienServices
+    {
+        public Task<APIResult<PageResult<QLDangVienViewModel>>> GetAllQLDangVien(GetQLDangVienPagingRequest request);
+    }
+}
---
+++ QLSQ.ViewModel/Catalogs/QLDangVien/QLDangVienViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLSQ.ViewModel.Catalogs.QLDangVien
+{
+    public class QLDangVienViewModel
+    {
+        public int IDSQ { get; set; }
+        public string HoTen { get; set; }
+        public DateTime NgayVaoDang { get; set; }
+        public string NoiVaoDang { get; set; }
+    }
+}
