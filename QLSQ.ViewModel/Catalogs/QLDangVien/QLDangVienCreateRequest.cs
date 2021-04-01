b808e09281d0be052a56834d58159c988c70f43e
dinhducbac <63286941+dinhducbac@users.noreply.github.com>
2021-04-01 17:19:33 +0700

    edit name show for request, view model

--- QLSQ.ViewModel/Catalogs/QLDangVien/QLDangVienCreateRequest.cs
+++ QLSQ.ViewModel/Catalogs/QLDangVien/QLDangVienCreateRequest.cs
@@ -1,14 +1,18 @@
 ﻿using QLSQ.ViewModel.Catalogs.SiQuan;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace QLSQ.ViewModel.Catalogs.QLDangVien
 {
     public class QLDangVienCreateRequest
     {
+        [Display(Name = "ID sĩ quan")]
         public int IDSQ { get; set; }
+        [Display(Name = "Ngày vào đảng")]
         public DateTime NgayVaoDang { get; set; }
+        [Display(Name = "Nơi vào đảng")]
         public string NoiVaoDang { get; set; }
         public List<SiQuanViewModel> siQuanViewModels { get; set; }
     }
--- QLSQ.ViewModel/Catalogs/SiQuan/SiQuanDeleteRequest.cs
+++ QLSQ.ViewModel/Catalogs/SiQuan/SiQuanDeleteRequest.cs
@@ -1,14 +1,16 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace QLSQ.ViewModel.Catalogs.SiQuan
 {
     public class SiQuanDeleteRequest
     {
-      
+        [Display(Name ="ID Sĩ Quan")]
         public int IDSQ { get; set; }
+        [Display(Name = "Họ và tên")]
         public string HoTen { get; set; }
     }
 }
