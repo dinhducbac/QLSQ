65174fb73070963abf4c29902a51eb99f81e6113
dinhducbac <63286941+dinhducbac@users.noreply.github.com>
2021-04-05 15:20:01 +0700

    .

---
+++ QLSQ.ViewModel/Catalogs/HeSoLuongTheoQuanHam/HeSoLuongTheoQuanHamViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLSQ.ViewModel.Catalogs.HeSoLuongTheoQuanHam
+{
+    public class HeSoLuongTheoQuanHamViewModel
+    {
+        public int IDHeSoLuongQH { get; set; }
+        public int IDQH { get; set; }
+        public string TenQH { get; set; }
+        public float HeSoLuong { get; set; }
+    }
+}
