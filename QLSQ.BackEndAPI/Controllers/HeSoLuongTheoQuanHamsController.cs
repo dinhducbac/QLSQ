6e05998f16c741fe2dc6cf703ace8f28317245c2
dinhducbac <63286941+dinhducbac@users.noreply.github.com>
2021-04-05 16:14:18 +0700

    done show HeSoLuongTheoQuanHam

---
+++ QLSQ.BackEndAPI/Controllers/HeSoLuongTheoQuanHamsController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QLSQ.Application.Catalog.HeSoLuongTheoQuanHam;
+using QLSQ.ViewModel.Catalogs.HeSoLuongTheoQuanHam;
+
+namespace QLSQ.BackEndAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HeSoLuongTheoQuanHamsController : ControllerBase
+    {
+        public readonly IHeSoLuongTheoQuanHamServices _heSoLuongTheoQuanHamServices;
+        public HeSoLuongTheoQuanHamsController(IHeSoLuongTheoQuanHamServices heSoLuongTheoQuanHamServices)
+        {
+            _heSoLuongTheoQuanHamServices = heSoLuongTheoQuanHamServices;
+        }
+        [HttpGet("paging")]
+        public async Task<IActionResult> GetAllWithPaging([FromQuery]GetPagingRequestHeSoLuongTheoQuanHam request)
+        {
+            var result = await _heSoLuongTheoQuanHamServices.GetAllWithPaging(request);
+            return Ok(result);
+        }
+    }
+}
