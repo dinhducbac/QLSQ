7c2d502f6195a95f3060eae2255082d01fee316d
dinhducbac <63286941+dinhducbac@users.noreply.github.com>
2021-04-06 10:44:30 +0700

    .

---
+++ QLSQ.ViewModel/Catalogs/BoPhan/BoPhanViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLSQ.ViewModel.Catalogs.BoPhan
+{
+    public class BoPhanViewModel
+    {
+        public int IDBP { get; set; }
+        public string TenBP { get; set; }
+    }
+}
