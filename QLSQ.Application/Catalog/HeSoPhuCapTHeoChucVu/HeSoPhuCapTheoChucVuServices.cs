688ef4821a7764bfe4bc32d1c9c24fb407ee4d73
dinhducbac <63286941+dinhducbac@users.noreply.github.com>
2021-04-07 13:54:35 +0700

    done show list HeSoPhuCapTHeoChucVu

--- QLSQ.Application/Catalog/HeSoPhuCapTHeoChucVu/HeSoPhuCapTheoChucVuServices.cs
+++ QLSQ.Application/Catalog/HeSoPhuCapTHeoChucVu/HeSoPhuCapTheoChucVuServices.cs
@@ -1,17 +1,53 @@
-﻿using QLSQ.ViewModel.Catalogs.HeSoPhuCapTheoChucVu;
+﻿using QLSQ.Data.EF;
+using QLSQ.Data.Entities;
+using QLSQ.ViewModel.Catalogs.HeSoPhuCapTheoChucVu;
 using QLSQ.ViewModel.Common;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace QLSQ.Application.Catalog.HeSoPhuCapTHeoChucVu
 {
     public class HeSoPhuCapTheoChucVuServices : IHeSoPhuCapTheoChucVuServices
     {
-        public Task<APIResult<PageResult<HeSoPhuCapTheoChucVuViewModel>>> GetAllWithPaging(GetHeSoPhuCapPagingRequest request)
+        public readonly QL_SiQuanDBContext _context;
+        public HeSoPhuCapTheoChucVuServices (QL_SiQuanDBContext context)
         {
-            
+            _context = context;
+        }
+        public async Task<APIResult<PageResult<HeSoPhuCapTheoChucVuViewModel>>> GetAllWithPaging(GetHeSoPhuCapPagingRequest request)
+        {
+            var query = from cv in _context.ChucVus
+                        join hspc in _context.HeSoPhuCapTheoChucVus
+                        on cv.IDCV equals hspc.IDCV
+                        select new HeSoPhuCapTheoChucVuViewModel 
+                        { 
+                            IDHeSoPhuCapCV = hspc.IDHeSoPhuCapCV,
+                            IDCV = cv.IDCV,
+                            TenCV = cv.TenCV,
+                            HeSoPhuCap = hspc.HeSoPhuCap
+                        };
+            var totalRow = await query.CountAsync();
+            var data = await query.Skip((request.pageIndex - 1) * request.pageSize)
+                .Take(request.pageSize)
+                .Select(x => new HeSoPhuCapTheoChucVuViewModel
+                {
+                    IDHeSoPhuCapCV = x.IDHeSoPhuCapCV,
+                    IDCV = x.IDCV,
+                    TenCV = x.TenCV,
+                    HeSoPhuCap = x.HeSoPhuCap
+                }).ToListAsync();
+            var pageResult = new PageResult<HeSoPhuCapTheoChucVuViewModel>()
+            {
+                TotalRecord = totalRow,
+                PageIndex = request.pageIndex,
+                PageSize = request.pageSize,
+                Items = data
+            };
+            return new APISuccessedResult<PageResult<HeSoPhuCapTheoChucVuViewModel>>(pageResult);
         }
     }
 }
