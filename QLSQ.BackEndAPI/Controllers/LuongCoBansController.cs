9edcbb7a4f6f60def5fabcc2feea2fb04aab6a1e
dinhducbac <63286941+dinhducbac@users.noreply.github.com>
2021-04-14 14:52:45 +0700

    done show LuongCoBan
    done show LuongCoBan

---
+++ QLSQ.BackEndAPI/Controllers/LuongCoBansController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using QLSQ.Application.Catalog.LuongCoBan;
+using QLSQ.ViewModel.Catalogs.LuongCoBan;
+
+namespace QLSQ.BackEndAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LuongCoBansController : ControllerBase
+    {
+        public readonly ILuongCoBanServices _luongCoBanServices;
+        public LuongCoBansController(ILuongCoBanServices luongCoBanServices)
+        {
+            _luongCoBanServices = luongCoBanServices;
+        }
+        [HttpGet("paging")]
+        public async Task<IActionResult> GetAllWithPaging([FromQuery]GetLuongCoBanPagingRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+            var result = await _luongCoBanServices.GetALLWithPaging(request);
+            if (result.ResultObj != null)
+                return Ok(result);
+            return BadRequest(result);
+        }
+    }
+}
