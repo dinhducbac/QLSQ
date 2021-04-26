21c0f6cedf1c2c7e3c77ede548f3ebfa242d02ed
dinhducbac <63286941+dinhducbac@users.noreply.github.com>
2021-04-26 15:59:32 +0700

    done show list QLQuanHam
    done show list QLQuanHam

---
+++ QLSQ.Application/Catalog/QLQuanHam/IQLQuanHamServices.cs
@@ -0,0 +1,14 @@
+﻿using QLSQ.ViewModel.Catalogs.QLQuanHam;
+using QLSQ.ViewModel.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QLSQ.Application.Catalog.QLQuanHam
+{
+    public interface IQLQuanHamServices
+    {
+        public Task<APIResult<PageResult<QLQuanHamViewModel>>> GetAllWithPaging(GetQLQuanHamPagingRequest request);
+    }
+}
---
+++ QLSQ.ViewModel/Catalogs/QLQuanHam/QLQuanHamViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace QLSQ.ViewModel.Catalogs.QLQuanHam
+{
+    public class QLQuanHamViewModel
+    {
+        [Display(Name ="ID quản lý quân hàm")]
+        public int IDQLQH { get; set; }
+        [Display(Name = "ID sĩ quan")]
+        public int IDSQ { get; set; }
+        [Display(Name = "Họ tên sĩ quan")]
+        public string HoTen { get; set; }
+        [Display(Name = "Quân hàm")]
+        public string TenQH { get; set; }
+        [Display(Name = "Tên hệ số lương quân hàm")]
+        public string TenHeSoLuongQH { get; set; }
+    }
+}
