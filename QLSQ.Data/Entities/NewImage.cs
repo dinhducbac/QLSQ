a5bae3952c5c1e6e37ce4bfbd01d713d532e9976
dinhducbac <63286941+dinhducbac@users.noreply.github.com>
2021-05-06 09:15:49 +0700

    add table New and NewImage in database
    add table New and NewImage in database

---
+++ QLSQ.Data/Entities/NewImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QLSQ.Data.Entities
+{
+    public class NewImage
+    {
+        public int NewImageID { get; set; }
+        public int NewID { get; set; }
+        public string ImagePath { get; set; }
+        public DateTime DateCreated { get; set; }
+        public long FileSize { get; set; }
+
+        public New New { get; set; }
+    }
+}
